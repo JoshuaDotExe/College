0c04ed58ea4adaa9471564e2cdf5bdebc214517b
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2023-02-07 16:01:29 +0000

    working buttons

--- CS230/Assignment 1/foodPy.js
+++ CS230/Assignment 1/foodPy.js
@@ -1,6 +1,10 @@
-let t1num=0;
-let t2num=0;
-let t3num=0;
-let t4num=0;
-let t5num=0;
-let t6num=0;
+let tierCount = (0,0,0,0,0,0);
+let dispValList = ("dispVal1", "dispVal2", "dispVal3", "dispVal4", "dispVal5", "dispVal6");
+
+/* Used to handle the button onclick events and direct to pyramid mod */
+function button_handler(modifier, target){
+    let tempNum = parseInt(document.getElementById(target).innerHTML);
+    if(modifier<0 && tempNum<=0){return;}
+    document.getElementById(target).innerHTML=String(tempNum+modifier);
+}
+
