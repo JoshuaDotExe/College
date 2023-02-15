855c590137995fff48e4252910daaac121c83096
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2023-02-15 15:05:36 +0000

    working commit

--- CS230/Assignment 1/foodPy.js
+++ CS230/Assignment 1/foodPy.js
@@ -3,7 +3,14 @@ let dispValList = ["dispVal1", "dispVal2", "dispVal3", "dispVal4", "dispVal5", "
 
 let aPyFractionList = [1, 0.1, 1, 0.1, 1, 1, 1, 1];
 
-// Used to 
+class Pyramid {
+    constructor(){
+        let tierCount = (0,0,0,0,0,0);
+        let dispValList = ["dispVal1", "dispVal2", "dispVal3", "dispVal4", "dispVal5", "dispVal6"];
+        let aPyFractionList = [1, 0.1, 1, 0.1, 1, 1, 1, 1];
+    }
+}
+
 function aPyGetAverageSize(){
     let total = 0;
     for(i=0; i<aPyFractionList.length; i++){
@@ -14,14 +21,14 @@ function aPyGetAverageSize(){
     return total/aPyFractionList.length;
 }
 
+/* Returns a string for row adjusted fraction sizes */
 function aPyBuildStr(){
     outStr = "";
     averageSize = aPyGetAverageSize();
 
-    // Ensures that the spacing beneath each of the top
-    // two layers is scales accurately with the rest of the pyramid
     for(i = 0; i<aPyFractionList.length; i++){
         if(i==1||i==3){
+            /* Ensures the gaps have a constant size that scales with the rest of the pyramid */
             outStr += averageSize/aPyFractionList.length;
         } else {
             outStr += (aPyFractionList[i]).toString();
@@ -33,7 +40,9 @@ function aPyBuildStr(){
 
 function aPyAdjustSize(modifier, rowIndex){
     aPyAdjustSize[rowIndex] += modifier;
-    document.getElementById("aPy").style.gridTemplateRows = aPyBuildStr();
+    buildStr = aPyBuildStr();
+    document.getElementById("aPy").style.gridTemplateRows = buildStr;
+    document.getElementById("buttonContainer").style.gridTemplateRows = buildStr;
 }
 
 /* Used to handle the button onclick events and direct to pyramid mod */
@@ -46,8 +55,12 @@ function button_handler(modifier, target, rowIndex){
     console.log(document.getElementById("aPy").style.gridTemplateRows);
 }
 
+
+
+
+
+
 function display_date_handler(){
-    console.log(document.getElementById("date_input").textContent);
+    document.getElementById("date_display_input").textContent = ("Input date: " + document.getElementById("date_input").value);
 }
 
-display_date_handler();
